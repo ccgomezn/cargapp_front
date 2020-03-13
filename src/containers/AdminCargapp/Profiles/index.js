454a2e7207c74987404347d7b2a595fbf8b1d935
jramr7 <jmramirezr7@gmail.com>
2020-03-13 12:50:14 -0500

    enter key for login

--- src/containers/AdminCargapp/Profiles/index.js
+++ src/containers/AdminCargapp/Profiles/index.js
@@ -1,117 +1,107 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import LayoutWrapper from '../../../components/utility/layoutWrapper.js';
-import {tableinfos} from './configs';
+import { tableinfos } from './configs';
 import SortView from '../../../components/custom/table/sortView';
 import PageHeader from '../../../components/utility/pageHeader';
 import IntlMessages from '../../../components/utility/intlMessages';
-import {Row, Col} from 'antd';
+import { Row, Col } from 'antd';
 import basicStyle from '../../../settings/basicStyle';
 import axios from "axios";
-import {Redirect} from 'react-router-dom'
-import {getProfiles, getUsers} from "../../../helpers/api/users";
+import { Redirect } from 'react-router-dom'
+import { getProfiles, getUsers } from "../../../helpers/api/users";
 import SecondaryButton from "../../../components/custom/button/secondary";
 
 export default class Profile extends Component {
 
 
-    constructor(props) {
-        super();
-        this.state = {
-            reload: false
-        }
-
+  constructor(props) {
+    super();
+    this.state = {
+      reload: false
     }
+  }
 
+  transformDataToMap(data, key) {
+    var dataTransformed = {};
+    
+    data.map((item) => {
+      dataTransformed[item.id] = item[key];
+      return item;
+    });
+    return dataTransformed
+  }
 
-    transformDataToMap(data, key) {
-        var dataTransformed = {};
-        data.map((item) => {
-            dataTransformed[item.id] = item[key];
+  componentWillMount() {
+    axios.all([getProfiles(), getUsers()])
+      .then((responses) => {
+        if (responses[0] !== undefined) {
+          let data_users = this.transformDataToMap(responses[1].data, 'email');
+
+          responses[0].data.map((item) => {
+            item.user = data_users[item.user_id];
             return item;
-        });
+          });
 
-        return dataTransformed
+          this.setState({
+            profiles: responses[0].data,
+          });
+        }
+      })
+  }
+
+
+  redirectAdd() {
+    this.props.history.push('/admin/profiles/add')
+  }
+
+  render() {
+    const { rowStyle, colStyle } = basicStyle;
+    const { reload } = this.state;
+
+    if (reload) {
+      return <Redirect to='/admin/profiles' />
     }
+    return (
+      <LayoutWrapper>
 
 
-    componentWillMount() {
-        axios.all([getProfiles(), getUsers()])
-            .then((responses) => {
-                if (responses[0] !== undefined) {
-                    let data_users = this.transformDataToMap(responses[1].data, 'email');
+        <Row style={rowStyle} gutter={18} justify="start" block>
+          <Col lg={24} md={24} sm={24} xs={24} style={colStyle}>
+            <Row gutter={12}>
+              <Col lg={18} md={24} sm={24} xs={24} style={colStyle}>
+                <PageHeader>
 
-                    responses[0].data.map((item) => {
-                        item.user = data_users[item.user_id];
-                        return item;
-                    });
+                  <h1>
+                    <IntlMessages id="profiles.title" />
 
+                  </h1>
+                </PageHeader>
+              </Col>
 
+              <Col lg={6} md={24} sm={24} xs={24} style={colStyle}>
+                <SecondaryButton
+                  message_id={"general.add"}
+                  style={{ width: '100%' }}
+                  onClick={() => this.redirectAdd()} />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={24} md={24} sm={24} xs={24} style={colStyle}>
+                {this.state && this.state.profiles &&
+                  <SortView tableInfo={tableinfos[1]} dataList={this.state.profiles} />
 
-
-                    this.setState({
-                        profiles: responses[0].data,
-                    });
 
 
                 }
+              </Col>
+            </Row>
+
+          </Col>
+        </Row>
 
 
-            })
-    }
-
-
-    redirectAdd() {
-        this.props.history.push('/admin/profiles/add')
-    }
-
-    render() {
-        const {rowStyle, colStyle} = basicStyle;
-        const {reload} = this.state;
-
-        if (reload) {
-            return <Redirect to='/admin/profiles'/>
-        }
-        return (
-            <LayoutWrapper>
-
-
-                <Row style={rowStyle} gutter={18} justify="start" block>
-                    <Col lg={24} md={24} sm={24} xs={24} style={colStyle}>
-                        <Row gutter={12}>
-                            <Col lg={18} md={24} sm={24} xs={24} style={colStyle}>
-                                <PageHeader>
-
-                                    <h1>
-                                        <IntlMessages id="profiles.title"/>
-
-                                    </h1>
-                                </PageHeader>
-                            </Col>
-
-                            <Col lg={6} md={24} sm={24} xs={24} style={colStyle}>
-                                <SecondaryButton
-                                    message_id={"general.add"}
-                                    style={{width: '100%'}}
-                                    onClick={() => this.redirectAdd()}/>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg={24} md={24} sm={24} xs={24} style={colStyle}>
-                                {this.state && this.state.profiles &&
-                                <SortView tableInfo={tableinfos[1]} dataList={this.state.profiles}/>
-
-
-
-                                }
-                            </Col>
-                        </Row>
-
-                    </Col>
-                </Row>
-
-
-            </LayoutWrapper>
-        );
-    }
+      </LayoutWrapper>
+    );
+  }
 }
-export {tableinfos};
+export { tableinfos };
